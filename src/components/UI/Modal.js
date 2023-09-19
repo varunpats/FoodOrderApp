0e33c6c153ba9f2c6b122453e7013867e32da330
varunpats <varunpatil2502@gmail.com>
2023-09-19 21:57:07 +0530

    Changes in popup

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,24 +1,26 @@
+import classes from './Modal.module.css';
 import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import classes from './Modal.module.css';
 
-const Backdrop = () => {
+const Backdrop = props => {
     return <div className={classes.backdrop} />
 }
 
 const ModalOverlay = props => {
-    return <div classes={classes.modal}>
-        <div>{props.children}</div>
+    return <div className={classes.modal}>
+        <div className={classes.content}>
+            {props.children}
+        </div>
     </div>
 }
 
-const portalElement = document.getElementById("overlays");
+const modalPortal = document.getElementById('overlays');
 
 const Modal = props => {
-    return <Fragment>
-        {/* {ReactDOM.createPortal(<Backdrop />, portalElement)} */}
-        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
-    </Fragment>
+    return (<Fragment>
+        {ReactDOM.createPortal(<Backdrop/>, modalPortal)}
+        {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, modalPortal)}
+    </Fragment>)
 }
 
 export default Modal;
